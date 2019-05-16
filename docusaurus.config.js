41f1aae7c6baf50463235b3e076254706a40a5ba
op07n <39182105+op07n@users.noreply.github.com>
2019-05-16 09:15:29 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,21 +1,15 @@
 const siteConfig = {
-  title: 'Endilie Y',
+  title: 'op07n Y',
   tagline: 'I love building things',
   url: 'https://op07n.github.io',
   baseUrl: '/',
   projectName: 'op07n.github.io',
-  organizationName: 'endiliey',
+  organizationName: 'op07n',
   themeConfig: {
     headerLinks: [
-      {href: 'https://forum.endiliey.com', label: 'Forum'},
+      {href: 'https://forum.op07n.com', label: 'Forum'},
       {href: 'https://github.com/op07n', label: 'GitHub'},
     ],
-    algolia: {
-      apiKey: '171b70d5e547d2101578da9a40c0d9f5',
-      indexName: 'endiliey',
-      algoliaOptions: {
-        facetFilters: ['language:en'], // LANGUAGE will be dynamically changed in lib/core/Site.js
-      },
     },
   },
   headerIcon: 'img/endilie.gif',
